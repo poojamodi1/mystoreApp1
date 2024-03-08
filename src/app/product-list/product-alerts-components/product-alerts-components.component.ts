462a5cf6d36301b429835903a9e73c8f00ea7719
poojamodi1 <modi.pooja03.com>
2024-03-08 00:03:41 -0500

    fourth commit

--- src/app/product-list/product-alerts-components/product-alerts-components.component.ts
+++ src/app/product-list/product-alerts-components/product-alerts-components.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter  } from '@angular/core';
+import { Product } from '../../product';
 
 @Component({
   selector: 'app-product-alerts-components',
-  standalone: true,
-  imports: [],
   templateUrl: './product-alerts-components.component.html',
   styleUrl: './product-alerts-components.component.css'
 })
 export class ProductAlertsComponentsComponent {
-
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
