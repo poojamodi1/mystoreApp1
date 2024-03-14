204d25be0710c89e3a07beb3fa4d55cf6f34fda8
poojamodi1 <modi.pooja03@gmail.com>
2024-03-14 15:17:43 -0400

    cart shopping screen changes.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Form, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -11,8 +12,20 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  })
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
+
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn('your order has been submited', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
 
